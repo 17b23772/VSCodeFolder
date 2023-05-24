9d35edee1ea49f1d2b565efd23924382d1a6a3e3
Beach <17b23772@marling.school>
2023-05-24 09:14:06 +0100

    Making the colour selector

--- ocr-project/cycle1.js
+++ ocr-project/cycle1.js
@@ -3,4 +3,14 @@ Array.from(divs).forEach(div =>{
     div.addEventListener('click', classToggler);
 });
 
-const colors = []
+const colors = ['.lake', '.tree'];
+let enumerator = 0;
+
+function classToggler() {
+    if (enumerator < colors.length +1) {
+        enumerator += 1; 
+    }
+    else {enumerator = 0};
+this.classList.add (colors[enumerator]);
+this.classList.remove (colors[enumerator-1]);
+}
